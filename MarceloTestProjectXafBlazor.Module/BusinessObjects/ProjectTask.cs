77fe07ea0bf15c5c91a70aa773066b1513daaf26
marcelo <jmarcelovolpe@gmail.com>
2020-12-11 19:01:44 -0300

    Initial commit

---
+++ MarceloTestProjectXafBlazor.Module/BusinessObjects/ProjectTask.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+
+namespace MarceloTestProjectXafBlazor.Module.BusinessObjects
+{
+    [NavigationItem("Planning")]
+    public class ProjectTask : BaseObject
+    {
+        public ProjectTask(Session session) : base(session) { }
+        string subject;
+        [Size(255)]
+        public string Subject
+        {
+            get { return subject; }
+            set { SetPropertyValue(nameof(Subject), ref subject, value); }
+        }
+        ProjectTaskStatus status;
+        public ProjectTaskStatus Status
+        {
+            get { return status; }
+            set { SetPropertyValue(nameof(Status), ref status, value); }
+        }
+        Person assignedTo;
+        public Person AssignedTo
+        {
+            get { return assignedTo; }
+            set { SetPropertyValue(nameof(AssignedTo), ref assignedTo, value); }
+        }
+        DateTime startDate;
+        public DateTime StartDate
+        {
+            get { return startDate; }
+            set { SetPropertyValue(nameof(startDate), ref startDate, value); }
+        }
+        DateTime endDate;
+        public DateTime EndDate
+        {
+            get { return endDate; }
+            set { SetPropertyValue(nameof(endDate), ref endDate, value); }
+        }
+        string notes;
+        [Size(SizeAttribute.Unlimited)]
+        public string Notes
+        {
+            get { return notes; }
+            set { SetPropertyValue(nameof(Notes), ref notes, value); }
+        }
+        Project project;
+        [Association]
+        public Project Project
+        {
+            get { return project; }
+            set { SetPropertyValue(nameof(Project), ref project, value); }
+        }
+    }
+    // ...
+}
---
+++ MarceloTestProjectXafBlazor.Module/BusinessObjects/Testimonial.cs
@@ -0,0 +1,49 @@
+﻿using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Xpo;
+using System;
+
+namespace MarceloTestProjectXafBlazor.Module.BusinessObjects
+{
+    [NavigationItem("Marketing")]
+    public class Testimonial : BaseObject
+    {
+        public Testimonial(Session session) : base(session)
+        {
+            createdOn = DateTime.Now;
+        }
+        string quote;
+        public string Quote
+        {
+            get { return quote; }
+            set { SetPropertyValue(nameof(Quote), ref quote, value); }
+        }
+        string highlight;
+        [Size(512)]
+        public string Highlight
+        {
+            get { return highlight; }
+            set { SetPropertyValue(nameof(Highlight), ref highlight, value); }
+        }
+        DateTime createdOn;
+        [VisibleInListView(false)]
+        public DateTime CreatedOn
+        {
+            get { return createdOn; }
+            internal set { SetPropertyValue(nameof(CreatedOn), ref createdOn, value); }
+        }
+        string tags;
+        public string Tags
+        {
+            get { return tags; }
+            set { SetPropertyValue(nameof(Tags), ref tags, value); }
+        }
+        Customer customer;
+        [Association("Customer-Testimonials")]
+        public Customer Customer
+        {
+            get { return customer; }
+            set { SetPropertyValue(nameof(Customer), ref customer, value); }
+        }
+    }
+}
